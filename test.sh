c3968295962c5eb48f43ce9baf2c5dac21b1f1ba
camjac251 <camjac251@users.noreply.github.com>
2020-12-02 10:22:12 -0600

    add verbose output on scripts

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Checking (no changes to system are made)... It will ask for your sudo password"
 set -x
-ansible-playbook install.yml -K --check
+ansible-playbook install.yml -K --check -v
